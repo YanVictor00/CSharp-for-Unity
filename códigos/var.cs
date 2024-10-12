a827620e6a88b3e345381127a9ef939fe99343f8
Yan Victor <yanvictor799@gmail.com>
2024-10-12 00:31:51 -0300

    Variáveis

---
+++ códigos/var.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{   
+    public ind vida = 45;
+    public float forca = 1.5f;
+    private bool estavivo = false;
+    string nomeDoPersonagem = "Yan";
+    GameObject player;
+    Rigidbody Rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
